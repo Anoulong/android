5f7297a5d0b883dfa0dd41caebc59a688b7b7c56
Anou Chanthavong <anoulong.chanthavong@quickseries.com>
2017-12-01 17:24:28 -0500

    create project

---
+++ ra-android/app/src/main/java/com/quickseries/ra/ResourceApplication.java
@@ -0,0 +1,20 @@
+package com.quickseries.ra;
+
+import android.app.Application;
+
+/*******************************************************************************
+ * QuickSeries® Publishing inc.
+ * <p>
+ * Copyright (c) 1992-2017 QuickSeries® Publishing inc.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of QuickSeries®
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with QuickSeries® and partnership.
+ * <p>
+ * Created by aac on 2017-12-01.
+ ******************************************************************************/
+public class ResourceApplication extends Application{
+
+}
