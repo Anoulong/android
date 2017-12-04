711ecbf6c5235c6f2ce7400cc1da68f194e8b104
Anou Chanthavong <anoulong.chanthavong@gmail.com>
2017-12-04 09:53:25 -0500

    add dependency injection framework

--- ra-android/app/src/main/java/com/quickseries/ra/ResourceApplication.java
+++ ra-android/app/src/main/java/com/quickseries/ra/ResourceApplication.java
@@ -1,6 +1,7 @@
 package com.quickseries.ra;
 
 import android.app.Application;
+import android.content.Context;
 
 /*******************************************************************************
  * QuickSeries® Publishing inc.
@@ -10,11 +11,33 @@ import android.app.Application;
  * <p>
  * This software is the confidential and proprietary information of QuickSeries®
  * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with QuickSeries® and partnership.
+ * Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with QuickSeries® and QuickSeries's Partners.
  * <p>
- * Created by aac on 2017-12-01.
+ * Created by Anou Chanthavong on 2017-12-01.
  ******************************************************************************/
 public class ResourceApplication extends Application{
+    private static final String TAG = ResourceApplication.class.getSimpleName();
 
+    private ApplicationComponent mApplicationComponent;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        ResourceApplication.getApplicationComponent(this).inject(this);
+
+    }
+
+    protected ApplicationModule getApplicationModule() {
+        return new ApplicationModule(this);
+    }
+
+    public static ApplicationComponent getApplicationComponent(Context context) {
+        ResourceApplication application = (ResourceApplication) context.getApplicationContext();
+        if (application.mApplicationComponent == null) {
+            application.mApplicationComponent = DaggerApplicationComponent.builder()
+                    .applicationModule(application.getApplicationModule())
+                    .build();
+        }
+        return application.mApplicationComponent;
+    }
 }
