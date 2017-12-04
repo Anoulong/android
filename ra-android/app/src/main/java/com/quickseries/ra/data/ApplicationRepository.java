cb6ea6bd5cd5f7cdeff021fe0c77a4331ae99e23
Anou Chanthavong <anoulong.chanthavong@gmail.com>
2017-12-04 09:54:33 -0500

    create package

---
+++ ra-android/app/src/main/java/com/quickseries/ra/data/ApplicationRepository.java
@@ -0,0 +1,17 @@
+package com.quickseries.ra.data;
+
+/*******************************************************************************
+ * QuickSeries® Publishing inc.
+ * <p>
+ * Copyright (c) 1992-2017 QuickSeries® Publishing inc.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of QuickSeries®
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with QuickSeries® and QuickSeries's Partners.
+ * <p>
+ * Created by Anou Chanthavong on 2017-12-02.
+ ******************************************************************************/
+public class ApplicationRepository {
+}
