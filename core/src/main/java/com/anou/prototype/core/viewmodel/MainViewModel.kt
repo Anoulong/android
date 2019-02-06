1f5a9b27d532b72974bf42480dfbb3c25e6de3df
Anoulong Chanthavong <anoulong.chanthavong@quickseries.com>
2019-02-05 20:32:20 -0500

    update code

--- core/src/main/java/com/anou/prototype/core/viewmodel/MainViewModel.kt
+++ core/src/main/java/com/anou/prototype/core/viewmodel/MainViewModel.kt
@@ -10,35 +10,35 @@ import com.anou.prototype.core.strategy.ResourceStatus
 import com.anou.prototype.core.usecase.SideMenuUseCase
 
 class MainViewModel(val dispatchers: AppCoroutineDispatchers, val applicationController: ApplicationController, val moduleRepository: ModuleRepository) : BaseViewModel() {
-    val liveModule = MutableLiveData<Boolean>()
-    val stateLiveData = MediatorLiveData<SideMenuUseCase>()
+    private val liveSource = MutableLiveData<Boolean>()
+    private val liveUseCase = MediatorLiveData<SideMenuUseCase>()
 
     fun getModules(): LiveData<SideMenuUseCase> {
 
-        val source = Transformations.switchMap(liveModule) {
+        val source = Transformations.switchMap(liveSource) {
             moduleRepository.loadModules()
         }
 
-        stateLiveData.addSource(source) { result ->
+        liveUseCase.addSource(source) { result ->
             when (result.status) {
                 ResourceStatus.LOADING,
                 ResourceStatus.FETCHING -> {
-                    stateLiveData.value = SideMenuUseCase.ShowLoading
+                    liveUseCase.value = SideMenuUseCase.ShowLoading
                     //                        showTransparentProgressDialog()
                 }
                 ResourceStatus.SUCCESS -> {
-                    stateLiveData.value = SideMenuUseCase.ShowSuccess("Bravo")
+                    liveUseCase.value = SideMenuUseCase.ShowSuccess("Bravo")
                     result.value?.let { data ->
-                        stateLiveData.value = SideMenuUseCase.SetData(data)
+                        liveUseCase.value = SideMenuUseCase.SetData(data)
 
                         if (data.size > 0) {
                             data.get(0).let { firstModule ->
-                                stateLiveData.value = SideMenuUseCase.InitializeModule(firstModule)
+                                liveUseCase.value = SideMenuUseCase.InitializeModule(firstModule)
                                 //                                mainRouter.onModuleSelected(activity as MainActivity, firstModule, true)
                             }
                         } else {
 
-                            stateLiveData.value = SideMenuUseCase.ShowEmpty("No modules")
+                            liveUseCase.value = SideMenuUseCase.ShowEmpty("No modules")
                             //                            Toast.makeText(activity, "", Toast.LENGTH_LONG).show()
                         }
 
@@ -46,14 +46,14 @@ class MainViewModel(val dispatchers: AppCoroutineDispatchers, val applicationCon
 
                     //initialize the first module as the landing screen
                     //                        dismissProgressDialog()
-                    stateLiveData.value = SideMenuUseCase.HideLoading
+                    liveUseCase.value = SideMenuUseCase.HideLoading
 
                 }
                 ResourceStatus.ERROR -> {
                     result.error?.message?.let { errorMessage ->
-                        stateLiveData.value = SideMenuUseCase.ShowError(errorMessage)
+                        liveUseCase.value = SideMenuUseCase.ShowError(errorMessage)
                     }
-                    stateLiveData.value = SideMenuUseCase.HideLoading
+                    liveUseCase.value = SideMenuUseCase.HideLoading
                     //                        dismissProgressDialog()
                 }
                 ResourceStatus.UNKNOWN,
@@ -63,11 +63,11 @@ class MainViewModel(val dispatchers: AppCoroutineDispatchers, val applicationCon
             }
         }
 
-        return stateLiveData
+        return liveUseCase
     }
 
     fun refresh(refresh: Boolean? = false) {
-        liveModule.value = refresh
+        liveSource.value = refresh
     }
 
 
